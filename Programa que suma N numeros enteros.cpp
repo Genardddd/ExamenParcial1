e956c9787d4064936a5ad8629ebec1e59fd1c584
Genardddd <141948382+Genardddd@users.noreply.github.com>
2023-09-05 14:21:00 -0600

    Add files via upload

---
+++ Programa que suma N numeros enteros.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//Programa que suma N números enteros.
+
+int main() {	
+int n;
+int suma=0;
+int i=1;
+
+printf("Ingrese un numero entero N: ");
+scanf("%d",&n);
+
+if(n<=0){
+printf("Por favor, ingrese un numero entero positivo.\n");	
+} else{
+	while (i<=n){
+	suma +=i;
+	i++;
+}
+printf("La suma de los enteros desde 1 hasta %d es: %d\n", n, suma);
+}
+return 0;
+}
