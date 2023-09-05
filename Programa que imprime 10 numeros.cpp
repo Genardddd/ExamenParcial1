dc5bd482df369909eab2d720f0ed1e8f1dea75ac
Genardddd <141948382+Genardddd@users.noreply.github.com>
2023-09-05 10:19:03 -0600

    Add files via upload

---
+++ Programa que imprime 10 numeros.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//Programa que imprime los 10 números siguientes a partir del que eliges.
+
+int main() {
+
+int numero;
+int contador=0;
+
+printf("¿A partir de que numero quieres contar?\n");
+scanf("%d", &numero);
+
+printf("Los 10 numeros siguientes a %d son\n", numero);
+
+while (contador<11){
+	printf("%d\n", numero);
+	numero++;
+	contador++;
+}
+
+return 0;
+}
+
