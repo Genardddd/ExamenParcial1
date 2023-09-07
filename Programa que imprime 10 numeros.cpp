c99bda1f1d218ec40659c9ddde68d10d63422296
Genardddd <141948382+Genardddd@users.noreply.github.com>
2023-09-07 00:38:42 -0600

    Programa que imprime 10 numeros.cpp

--- Programa que imprime 10 numeros.cpp
+++ Programa que imprime 10 numeros.cpp
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-//Programa que imprime los 10 números siguientes a partir del que eliges.
+//Programa que imprime los 10 nÃºmeros siguientes a partir del que eliges.
+//Genaro Bermudez. 179588
 
 int main() {
 
 int numero;
 int contador=0;
 
-printf("¿A partir de que numero quieres contar?\n");
+printf("Â¿A partir de que numero quieres contar?\n");
 scanf("%d", &numero);
 
 printf("Los 10 numeros siguientes a %d son\n", numero);
